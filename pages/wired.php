7b01fe16b2562c965d75fb4569277e05239a1a31
Sergey-hub02 <s.park190802@gmail.com>
2022-12-02 18:02:40 +0300

    Добавил основные страницы для сайта

--- pages/wired.php
+++ pages/wired.php
@@ -6,7 +6,7 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Главная</title>
+  <title>Проводной геймпад</title>
   <link rel="stylesheet" href="/libs/bootstrap-5.2.3-dist/css/bootstrap.min.css">
 </head>
 
